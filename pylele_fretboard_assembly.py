f95611dfcf9c4930b3101b66760e6776b983fa11
Marco Merlin <marcomerli@gmail.com>
2024-08-03 14:50:13 +0200

    add using nails as fret option

--- pylele_fretboard_assembly.py
+++ pylele_fretboard_assembly.py
@@ -5,6 +5,8 @@
 """
 
 import os
+import argparse
+from enum import Enum
 from pylele_api import Shape
 from pylele_base import LeleBase
 from pylele_config import Implementation, FILLET_RAD
@@ -15,6 +17,28 @@ from pylele_top import LeleTop
 from pylele_strings import LeleStrings
 from pylele_fretboard_spines import LeleFretboardSpines
 from pylele_fretboard_joint import LeleFretboardJoint
+
+class FretType(Enum):
+    PRINT = 'print'
+    NAIL = 'nail'
+
+    def __str__(self):
+        return self.value
+
+def pylele_fretboard_assembly_parser(parser = None):
+    """
+    Pylele Fretboard Assembly Parser
+    """
+    if parser is None:
+        parser = argparse.ArgumentParser(description='Pylele Configuration')
+
+    parser.add_argument("-ft", "--fret_type",
+                    help="Fret Type",
+                    type=FretType,
+                    choices=list(FretType),
+                    default=FretType.PRINT
+                    )
+    return parser
 class LeleFretboardAssembly(LeleBase):
     """ Pylele Fretboard Assembly Generator class """
 
@@ -26,9 +50,16 @@ class LeleFretboardAssembly(LeleBase):
         fdotsCut = LeleFretboardDots(isCut=True,cli=self.cli)
         topCut = LeleTop(isCut=True,cli=self.cli).mv(0, 0, -self.cfg.joinCutTol) if self.cfg.sepFretbd or self.cfg.sepNeck else None
 
-        fbJoiners = [frets]
+        fbJoiners = []
         fbCutters = [fdotsCut, strCuts]
         fbFillets = {}
+
+        if self.cli.fret_type == FretType.PRINT:
+            fbJoiners.append(frets)
+        elif self.cli.fret_type == FretType.NAIL:
+            fbCutters.append(frets)
+        else:
+            assert f'Unsupported FretType: {self.cli.fret_type} !'
 
         if self.cfg.sepFretbd or self.cfg.sepNeck:
             fbCutters.insert(0, topCut)
@@ -54,10 +85,17 @@ class LeleFretboardAssembly(LeleBase):
         self.shape = fretbd.shape
 
         return self.shape
+    
+    def gen_parser(self,parser=None):
+        """
+        pylele Command Line Interface
+        """
+        return super().gen_parser( parser=pylele_fretboard_assembly_parser(parser=parser) )
 
 def fretboard_assembly_main(args=None):
     """ Generate Fretboard Assembly """
     solid = LeleFretboardAssembly(args=args)
+    # solid.cli = solid.parse_args(args=args)
     solid.export_args() # from cli
     solid.export_configuration()
     solid.exportSTL()
@@ -70,13 +108,15 @@ def test_fretboard_assembly():
     tests = {
         'separate_fretboard' : ['-F'],
         'cadquery': ['-i','cadquery'],
-        'blender' : ['-i','blender']
+        'blender' : ['-i','blender'],
+        'fret_nails': ['-ft', str(FretType.NAIL) ]
     }
 
     for test,args in tests.items():
         print(f'# Test {component} {test}')
         outdir = os.path.join('./test',component,test)
         args += ['-o', outdir]
+        # print(args)
         fretboard_assembly_main(args=args)
 
 if __name__ == '__main__':
