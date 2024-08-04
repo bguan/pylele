a51e1d2142206b33b37da1f2b102b49b89f9e2a6
Marco Merlin <marcomerli@gmail.com>
2024-08-04 23:23:06 +0200

    remove zero-fret from frets

--- pylele_frets.py
+++ pylele_frets.py
@@ -31,13 +31,17 @@ class LeleFrets(LeleBase):
         riseAng = self.cfg.fretbdRiseAng
         f0X = -fitTol if self.isCut else 0
 
-        f0Top = self.api.genRndRodY(ntWth, ntHt, 1/4)
-        f0TopCut = self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, -fbTck/2)
-        f0Top = f0Top.cut(f0TopCut).mv(f0X, 0, fbTck)
-        f0Bot = self.api.genRndRodY(ntWth, ntHt, 1/4)
-        f0BotCut = self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, fbTck/2)
-        f0Bot = f0Bot.cut(f0BotCut).scale(1, 1, fbTck/ntHt).mv(f0X, 0, fbTck)
-        frets = f0Top.join(f0Bot)
+        if False:
+            # zero-fret
+            f0Top = self.api.genRndRodY(ntWth, ntHt, 1/4)
+            f0TopCut = self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, -fbTck/2)
+            f0Top = f0Top.cut(f0TopCut).mv(f0X, 0, fbTck)
+            f0Bot = self.api.genRndRodY(ntWth, ntHt, 1/4)
+            f0BotCut = self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, fbTck/2)
+            f0Bot = f0Bot.cut(f0BotCut).scale(1, 1, fbTck/ntHt).mv(f0X, 0, fbTck)
+            frets = f0Top.join(f0Bot)
+        else:
+            frets =  None
 
         if not self.isCut:
             fx = 0
@@ -48,7 +52,10 @@ class LeleFrets(LeleBase):
                 fy = fWth / 2 + math.tan(radians(wideAng)) * fx
                 fz = fbTck + math.tan(radians(riseAng)) * fx
                 fret = self.api.genRodY(2 * fy, fHt).mv(fx, 0, fz)
-                frets = frets.join(fret)
+                if frets is None:
+                    frets = fret
+                else:
+                    frets = frets.join(fret)
                 gap = gap / SEMI_RATIO
                 count += 1
                 if (count > maxFrets):  # prevent runaway loop
