4ec15a1117a8777048dafcf7d6168ee986746bdb
Marco Merlin <marcomerli@gmail.com>
2024-09-06 16:52:33 +0200

    enable test of all assemblies with all apis

--- pylele2/pylele_all_assembly.py
+++ pylele2/pylele_all_assembly.py
@@ -85,7 +85,7 @@ def main(args=None):
                       args=args)
 
 
-def test_all_assembly(self,apis=['cadquery']):
+def test_all_assembly(self,apis=None):
     """ Test All Assembly """
 
     tests = {
