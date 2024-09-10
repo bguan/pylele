3c1b144a0a302a19e18b12340b05ef6c8e1cc197
Marco Merlin <marcomerli+github@gmail.com>
2024-09-10 19:07:36 +0000

    move pip requiredements in install_dependencies before install fonts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,11 +4,11 @@
 sudo apt update 
 sudo apt install python3-pip libgl1 -y
 
+# update pip
+python3 -m pip install --upgrade pip
+# python dependencies
+pip install -r requirements.txt
+
 # install fonts
 sudo apt install ttf-mscorefonts-installer -y
 sudo fc-cache -f
-
-# update pip
-# python3 -m pip install --upgrade pip
-# python dependencies
-# pip install -r requirements.txt
