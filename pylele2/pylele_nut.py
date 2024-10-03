1740505ebe8c016d84bd7734f7ebfdbdbac53d81
Marco Merlin <marcomerli@gmail.com>
2024-10-04 00:13:05 +0200

    simplified nut

--- pylele2/pylele_nut.py
+++ pylele2/pylele_nut.py
@@ -10,10 +10,8 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
-from api.pylele_api import LeleStrEnum, Shape
-from api.pylele_api_constants import FIT_TOL
-from api.pylele_solid import main_maker, test_loop
-from pylele2.pylele_base import LeleBase
+from api.pylele_api import Shape, Direction
+from pylele2.pylele_base import LeleBase, LeleStrEnum, test_loop, main_maker, FIT_TOL
 from pylele2.pylele_strings import LeleStrings
 
 
@@ -51,21 +49,22 @@ class LeleNut(LeleBase):
         fitTol = FIT_TOL
         fbTck = self.cfg.FRETBD_TCK
         ntHt = self.cfg.NUT_HT
-        ntWth = self.cfg.nutWth + fbTck / 4 + 0.5  # to be wider than fretbd
+        ntWth = self.cfg.nutWth + fbTck/4 + .5  # to be wider than fretbd
         f0X = -fitTol if self.isCut else 0
 
-        f0Top = self.api.genRndRodY(ntWth, ntHt, 1 / 4)
-        f0TopCut = self.api.genBox(2 * ntHt, 2 * ntWth, fbTck).mv(0, 0, -fbTck / 2)
-        f0Top = f0Top.cut(f0TopCut).mv(f0X, 0, fbTck)
-        f0Bot = self.api.genRndRodY(ntWth, ntHt, 1 / 4)
-        f0BotCut = self.api.genBox(2 * ntHt, 2 * ntWth, fbTck).mv(0, 0, fbTck / 2)
-        f0Bot = f0Bot.cut(f0BotCut).scale(1, 1, fbTck / ntHt).mv(f0X, 0, fbTck)
-        nut = f0Top.join(f0Bot)
+        f0Top    = self.api.genRndRodY(ntWth, ntHt, 1/4)
+        f0Top   -= self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, -fbTck/2)
+
+        f0Bot    = self.api.genRndRodY(ntWth, ntHt, 1/4)
+        f0Bot   -= self.api.genBox(2*ntHt, 2*ntWth, fbTck).mv(0, 0, fbTck/2)
+        f0Bot   *= Direction(z=fbTck/ntHt)
+        
+        nut = f0Top + f0Bot
+        nut  <<= (f0X, 0, fbTck)
 
         # Add strings cut
-        if not self.cli.nut_type == NutType.ZEROFRET and not self.isCut:
-            strings = LeleStrings(isCut=True, cli=self.cli)
-            nut = nut.cut(strings.gen_full())
+        if not self.cli.nut_type == NutType.ZEROFRET: # and not self.isCut:
+            nut -= LeleStrings(isCut=True,cli=self.cli).gen_full()
 
         return nut
 
