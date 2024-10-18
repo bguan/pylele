904d3fad2810a207ad072d2cd5f8b1e2e202f63a
Marco Merlin <marcomerli@gmail.com>
2024-10-19 00:20:38 +0200

    fillet fretboard edges

--- pylele2/pylele_fretboard.py
+++ pylele2/pylele_fretboard.py
@@ -5,10 +5,11 @@
 """
 import os
 import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+import math
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 from api.pylele_api import Shape
+from api.pylele_utils import degrees
 from pylele2.pylele_base import LeleBase, test_loop, main_maker, FIT_TOL, FILLET_RAD
 
 class LeleFretboard(LeleBase):
@@ -18,11 +19,17 @@ class LeleFretboard(LeleBase):
         """Generate Fretboard"""
 
         cutAdj = FIT_TOL if self.isCut else 0
-        fbLen = self.cfg.fretbdLen + 2 * cutAdj
-        fbWth = self.cfg.fretbdWth + 2 * cutAdj
-        fbTck = self.cfg.FRETBD_TCK + 2 * cutAdj
-        fbHt = self.cfg.fretbdHt + 2 * cutAdj
+        fbLen = self.cfg.fretbdLen + 2*cutAdj
+        fbWth = self.cfg.fretbdWth + 2*cutAdj
+        fbTck = self.cfg.FRETBD_TCK + 2*cutAdj
+        fbHt = self.cfg.fretbdHt + 2*cutAdj
+        nut_width = self.cfg.nutWth
         riseAng = self.cfg.fretbdRiseAng
+        wide_angle = degrees(
+                                math.atan(
+                                            (fbWth-nut_width)/(2*fbLen)
+                                )
+                        )
 
         # path = self.cfg.fbCutPath if self.isCut else self.cfg.fbPath
         path = self.cfg.genFbPath(isCut=self.isCut)
@@ -37,16 +44,24 @@ class LeleFretboard(LeleBase):
             fretbd -= topCut
 
             ## fillet the end of the fretboard
-            # fretbd = fretbd.filletByNearestEdges([(fbLen, 0, fbHt)], fbTck/2)
             fretbd -= self.api.gen_rounded_edge_mask(direction='y',l=fbWth,rad=fbTck/2,rot=270)\
                 .mv(fbTck/2,0,fbTck/2)
 
             ## fillet the start of the fretboard
-            # fretbd = fretbd.filletByNearestEdges(  
-            #    rad = FILLET_RAD, nearestPts=[(self.cfg.fretbdLen, 0, .5*self.cfg.fretbdHt)]
-            # )
             fretbd -= self.api.gen_rounded_edge_mask(direction='y',l=fbWth,rad=FILLET_RAD,rot=0)\
                 .mv(fbLen-FILLET_RAD,0,fbHt-FILLET_RAD)
+            
+            ## fillet the fretboard sides
+            fretbd -= self.api.gen_rounded_edge_mask(direction='x',l=fbLen,rad=fbTck/2,rot=0)\
+                .rotateY(-riseAng)\
+                .rotateZ( wide_angle)\
+                .mv( fbLen/2, fbWth/2 - fbTck- fbTck/2, fbHt/2)
+
+            ## fillet the fretboard sides
+            fretbd -= self.api.gen_rounded_edge_mask(direction='x',l=fbLen,rad=fbTck/2,rot=90)\
+                .rotateY(-riseAng)\
+                .rotateZ(-wide_angle)\
+                .mv( fbLen/2, -fbWth/2 + fbTck+ fbTck/2, fbHt/2)
 
         return fretbd
 
