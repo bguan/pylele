f9569ab075100c30585337afcf98f7467cbf59e5
Marco Merlin <marcomerli@gmail.com>
2024-11-03 10:02:09 +0100

    add reference volumes to the body configurations

--- pylele2/pylele_config.py
+++ pylele2/pylele_config.py
@@ -28,11 +28,11 @@ WORM    = ['-t','worm'   ,'-e','90','-E','-wah','-wsl','35','-whk']
 BIGWORM = ['-t','bigworm','-e','125','-E','-wah','-wsl','35','-whk','-fbt','35']
 
 CONFIGURATIONS = {
-        'default'        : [],
-        'worm'           : WORM, # gourd
-        'flat'           : WORM    + ['-bt', LeleBodyType.FLAT],
-        'hollow'         : BIGWORM + ['-bt', LeleBodyType.HOLLOW],
-        'travel'         : BIGWORM + ['-bt', LeleBodyType.TRAVEL, '-wt', '25']
+        'default'        : ['-refv','1747826'],
+        'worm'           : WORM    + ['-refv','1760619'], # gourd
+        'flat'           : WORM    + ['-bt', LeleBodyType.FLAT,'-refv','1511594'],
+        'hollow'         : BIGWORM + ['-bt', LeleBodyType.HOLLOW,'-refv','2287141'],
+        'travel'         : BIGWORM + ['-bt', LeleBodyType.TRAVEL,'-w','25','-refv','1516693']
     }
 
 class AttrDict(dict):
