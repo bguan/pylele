f538f87e33d820bb8adb2c627933c01ebd5cc3fa
Marco Merlin <marcomerli@gmail.com>
2024-11-19 01:25:16 +0100

    fix paths in parts

--- src/pylele/parts/scad_example.py
+++ src/pylele/parts/scad_example.py
@@ -6,7 +6,7 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 from pylele.api.solid import Solid, test_loop, main_maker, Implementation, DEFAULT_TEST_DIR
 from pylele.api.core import Shape
