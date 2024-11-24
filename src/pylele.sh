e47f4b060241f97f1202b5884c85fddfc07c9154
bat52 <marcomerli@gmail.com>
2024-11-24 09:23:56 +0100

    fix scripts execution path

--- src/pylele.sh
+++ src/pylele.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname $0`
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 LOG=pylele.log
 time python3 -m cProfile -s tottime $SCRIPT_DIR/pylele/pylele1/main.py $@ > $LOG
 head $LOG
