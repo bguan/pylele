1ae46617706f364d4fcebff97dc9c069c17871da
bat52 <marcomerli@gmail.com>
2025-01-03 10:30:32 +0100

    separate b1scad test

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
-time python3 $SCRIPT_DIR/b1scad/test.py
+# time python3 $SCRIPT_DIR/b1scad/test.py
 time python3 $SCRIPT_DIR/pylele/test.py $@
